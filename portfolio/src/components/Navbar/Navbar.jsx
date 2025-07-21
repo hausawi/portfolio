3df5cd0baf88c4b193e9589956152b228f79fd9d
hausawi <haidermoubarak1987@gmail.com>
2025-07-21 21:34:45 +0200

    setup cv download file and modify textarea text-color

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import './Navbar.css';
 import wave1 from '../../assets/wave1.png';
-import arow from '../../assets/arow.png';
 import logo from '../../assets/logo.png';
 import close from '../../assets/close.png';
 import open from '../../assets/open.png';
@@ -28,41 +27,45 @@ const Navbar = () => {
 					<AnchorLink className='anchor-link' href='#home'>
 						<p onClick={() => setMenu('home')}>Home</p>
 					</AnchorLink>
-					{menu === 'home' ? <img src={wave1} alt='' className='img1' />  : <></>}
+					{menu === 'home' ? (
+						<img src={wave1} alt='' className='img1' />
+					) : (
+						<></>
+					)}
 				</li>
 				<li>
 					{' '}
 					<AnchorLink className='anchor-link' offset={50} href='#about'>
 						<p onClick={() => setMenu('about')}>About Me</p>
 					</AnchorLink>
-					{menu === 'about' ? <img src={wave1} alt=''  />  : <></>}
+					{menu === 'about' ? <img src={wave1} alt='' /> : <></>}
 				</li>
 				<li>
 					{' '}
 					<AnchorLink className='anchor-link' offset={50} href='#services'>
 						<p onClick={() => setMenu('services')}>Services</p>
 					</AnchorLink>
-					{menu === 'services' ? <img src={wave1} alt=''  />  : <></>}
+					{menu === 'services' ? <img src={wave1} alt='' /> : <></>}
 				</li>
 				<li>
 					{' '}
 					<AnchorLink className='anchor-link' offset={50} href='#work'>
 						<p onClick={() => setMenu('work')}>Portfolio</p>
 					</AnchorLink>
-					{menu === 'work' ? <img src={wave1} alt=''  />  : <></>}
+					{menu === 'work' ? <img src={wave1} alt='' /> : <></>}
 				</li>
 				<li>
 					{' '}
 					<AnchorLink className='anchor-link' offset={50} href='#contact'>
 						<p onClick={() => setMenu('contact')}>Contact</p>
 					</AnchorLink>
-					{menu === 'contact' ? <img src={wave1} alt=''  />  : <></>}
+					{menu === 'contact' ? <img src={wave1} alt='' /> : <></>}
 				</li>
 			</ul>
 			<div className='nav-connect'>
-				<AnchorLink className='anchor-link' href='#contact'>
-					Connect With Me
-				</AnchorLink>
+				<a href='Haider_Hausawi_CV.pdf' download='Haider M. Hausawi CV'>
+					Download CV
+				</a>
 			</div>
 		</div>
 	);
